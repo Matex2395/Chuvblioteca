b2dbda4a2d85be2b4b1046e57a72bd0f841143c3
Matex2395 <mateorgafjo@gmail.com>
2025-06-06 12:53:39 -0500

    Agregar atributos clase Autor

--- src/main/java/com/chuvblocks/chuvblioteca/modelo/Autor.java
+++ src/main/java/com/chuvblocks/chuvblioteca/modelo/Autor.java
@@ -1,5 +1,19 @@
 package com.chuvblocks.chuvblioteca.modelo;
 
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
 public class Autor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    @Required
+    private String nombre;
 }
