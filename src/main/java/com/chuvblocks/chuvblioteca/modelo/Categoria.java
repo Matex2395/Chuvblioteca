96361ce0d0abc953493e8270188206662ad3064c
Matex2395 <mateorgafjo@gmail.com>
2025-06-06 12:58:18 -0500

    Agregar atributos clase Categoría

--- src/main/java/com/chuvblocks/chuvblioteca/modelo/Categoria.java
+++ src/main/java/com/chuvblocks/chuvblioteca/modelo/Categoria.java
@@ -1,5 +1,19 @@
 package com.chuvblocks.chuvblioteca.modelo;
 
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    @Required
+    private String nombre;
 }
