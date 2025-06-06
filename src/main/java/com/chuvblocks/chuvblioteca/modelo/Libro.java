e259b7ccd1385495ef19afadb25054fb7f7d8e14
Matex2395 <mateorgafjo@gmail.com>
2025-06-06 13:15:51 -0500

    Agregar atributos clase Libro

--- src/main/java/com/chuvblocks/chuvblioteca/modelo/Libro.java
+++ src/main/java/com/chuvblocks/chuvblioteca/modelo/Libro.java
@@ -1,5 +1,30 @@
 package com.chuvblocks.chuvblioteca.modelo;
 
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
+@View(members = "titulo, autor, categoria, stock")
 public class Libro {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 150)
+    @Required
+    private String titulo;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Autor autor;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Categoria categoria;
+
+ // La logica de control de stock se implementará en el segundo avance
+    @Required
+    private int stock; 
 }
