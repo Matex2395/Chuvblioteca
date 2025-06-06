8f25d675d143fec2e4ab60f02117b8aeff75f604
Matex2395 <mateorgafjo@gmail.com>
2025-06-06 13:29:25 -0500

    Agregar atributos clase Factura

--- src/main/java/com/chuvblocks/chuvblioteca/modelo/Factura.java
+++ src/main/java/com/chuvblocks/chuvblioteca/modelo/Factura.java
@@ -1,5 +1,34 @@
 package com.chuvblocks.chuvblioteca.modelo;
 
+import java.math.*;
+import java.time.*;
+import java.util.*;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
 public class Factura {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Required
+    private LocalDate fecha;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Cliente cliente;
+
+    @ElementCollection
+    @ListProperties("libro.titulo, libro.autor.nombre, libro.categoria.nombre, cantidad, precioUnitario, subtotal")
+    private Collection<DetalleFactura> detalles;
+
+    @Stereotype("DINERO")
+    @ReadOnly
+    @Calculation("sum(detalles.subtotal)")
+    private BigDecimal total;
 }
