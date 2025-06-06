112dd1d493be01e6e6048dcfe936c0e2dd3ea857
Matex2395 <mateorgafjo@gmail.com>
2025-06-06 13:56:29 -0500

    Agregar atributos clase DetalleFactura

--- src/main/java/com/chuvblocks/chuvblioteca/modelo/DetalleFactura.java
+++ src/main/java/com/chuvblocks/chuvblioteca/modelo/DetalleFactura.java
@@ -1,5 +1,28 @@
 package com.chuvblocks.chuvblioteca.modelo;
 
+import java.math.*;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Embeddable @Getter @Setter
 public class DetalleFactura {
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Libro libro;
+
+    @Required
+    private int cantidad;
+
+    @Stereotype("DINERO")
+    @Required
+    private BigDecimal precioUnitario;
+
+    @Stereotype("DINERO")
+    @ReadOnly
+    @Calculation("cantidad * precioUnitario")
+    private BigDecimal subtotal;
 }
