1302989b08b954bd92cfadbdafe99408c1781b6e
Matex2395 <mateorgafjo@gmail.com>
2025-06-06 15:04:15 -0500

    Configurar conexión con Base de Datos
    Se ha añadido una integración con una base de datos en un servidor MySQL. Solo se ha comentado y quitado el comentario de ciertas partes del código.

--- src/main/java/com/chuvblocks/chuvblioteca/run/chuvblioteca.java
+++ src/main/java/com/chuvblocks/chuvblioteca/run/chuvblioteca.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class chuvblioteca {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("chuvblioteca-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		// DBServer.start("chuvblioteca-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
 		AppServer.run("chuvblioteca"); // Usa AppServer.run("") para funcionar en el contexto raíz
 	}
 
