84c549d138d6d6a9141be629b8b8dff664a31ec5
Matex2395 <mateorgafjo@gmail.com>
2025-06-05 23:09:53 -0500

    Creación de Proyecto
    Se crea el proyecto OpenXava para poder implementar sus clases.

---
+++ src/main/java/com/chuvblocks/chuvblioteca/run/chuvblioteca.java
@@ -0,0 +1,18 @@
+package com.chuvblocks.chuvblioteca.run;
+
+import org.openxava.util.*;
+
+/**
+ * Ejecuta esta clase para arrancar la aplicación.
+ *
+ * Con OpenXava Studio/Eclipse: Botón derecho del ratón > Run As > Java Application
+ */
+
+public class chuvblioteca {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("chuvblioteca-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		AppServer.run("chuvblioteca"); // Usa AppServer.run("") para funcionar en el contexto raíz
+	}
+
+}
