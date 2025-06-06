96f80ed50ed48879f8bce38ab355ec18ad46b581
Matex2395 <mateorgafjo@gmail.com>
2025-06-06 13:05:00 -0500

    Agregar atributos clase Cliente

--- src/main/java/com/chuvblocks/chuvblioteca/modelo/Cliente.java
+++ src/main/java/com/chuvblocks/chuvblioteca/modelo/Cliente.java
@@ -1,5 +1,25 @@
 package com.chuvblocks.chuvblioteca.modelo;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    @Required
+    private String nombre;
+
+    @Column(length = 100)
+    @Required
+    @Email
+    private String email;
 }
